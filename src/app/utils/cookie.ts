1f2e8ecce802b4b3187cd644cf917337470bc12f
Beomjun Kim <nutyworks@gmail.com>
2023-09-28 07:30:10 +0900

    Set cookie expire date to 9999. 12.

--- src/app/utils/cookie.ts
+++ src/app/utils/cookie.ts
@@ -3,7 +3,7 @@ import { Cookies } from "react-cookie";
 const cookies = new Cookies();
 
 export const setCookie = (name: string, value: any, option?: any) => {
-  return cookies.set(name, value, { ...option, expires: "never" });
+  return cookies.set(name, value, { ...option, expires: new Date(9999, 11)});
 }
 
 export const getCookie = (name: string, doNotParse: boolean = false) => {
